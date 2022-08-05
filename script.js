3a53daeb6135266565dbd804b92e5c1dccb6d839
Amelia Baldwin <amelia01000010@gmail.com>
2022-08-05 13:33:22 +0100

    convertDaysToAge function added

--- script.js
+++ script.js
@@ -3,11 +3,19 @@
 
 isNumberPositive = (num) =>{
     if(num >= 0){
-        return console.log("True")
+        return console.log("True");
     }else if(num <0){
-        return console.log("False")
+        return console.log("False");
     } 
 }
-
 isNumberPositive(-1); // returns false
 isNumberPositive(10); // returns true
+
+//Write a function that takes a number of days 
+//and converts it into an age.
+
+convertDaysToAge = (num) =>{
+    return Math.floor(num/365);
+}
+convertDaysToAge(3650); // returns 10
+convertDaysToAge(6570); // returns 18
